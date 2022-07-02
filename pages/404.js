7bc4947728a47894316443d39d075bb61fce7549
Juan Manuel <jmanuelgrehuello@yahoo.com>
2022-07-02 19:52:28 -0300

    404 actualizado

--- pages/404.js
+++ pages/404.js
@@ -2,17 +2,17 @@ import Layout from "../components/Layout";
 import Link from "next/link";
 
 const custom404 = () => (
-    <Layout title="Page Not Found" className="text-center">
+    <Layout>
         <div className="text-center">
-            <h1 className="display-1">
-                404
+            <h1 className="display-1 text-dark">
+                404 - Page not found.
             </h1>
-                <p>
-                    Developing this page at the moment. It will be online soon. Please, return to{" "} 
+            <p className="text-dark">
+                Developing this page at the moment. It will be online soon. Please, return to{" "} 
                 <Link href="/">
-                <a>
-                    Home
-                </a>
+                    <a className="text-dark">
+                        Home
+                    </a>
                 </Link>
             </p>
         </div>
