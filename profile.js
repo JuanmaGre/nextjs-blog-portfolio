be50c9c8abac064293987c66eb00976537c5d1af
Juan Manuel <jmanuelgrehuello@yahoo.com>
2023-02-10 15:05:13 -0300

    cambios en proyectos

--- profile.js
+++ profile.js
@@ -95,20 +95,20 @@ export const projects = [
         mainURL: 'https://revus.store'
     },
     {
-        name: "Pretty Dogs by Henry",
-        description: "Development of a SPA (Single Page Application) using React for the Front End and Redux as state management. The SPA obtains data from an API (through a Back End developed in Node.JS using Express), and also from a database managed with PostgreSQL.",
-        image: "landing.jpg",
-        from: "January 2022",
-        to: "February 2022",
-        mainURL: '/projects'
-    },
-    {
         name: "Henry Shoes",
         description: "Development of an E-COMMERCE of sneakers using stock of products, shopping cart through a LocalStorage with payment gateway integration with Stripe. The database was made with Mongo and each user logs into the page through Auth0 authentication.",
         image: "home 1.jpg",
         from: "February 2022",
         to: "March 2022",
         mainURL: 'https://practical-hopper-f19b68.netlify.app/'
+    },
+    {
+        name: "Netflix Clone",
+        description: "Netflix clone using NextJs with powerful user authentication, subscriptions to different plans, payments with stripe, completely dynamic and responsive for all devices. You can also play real movie's trailers and add movies to your list",
+        image: "netflix.jpg",
+        from: "July 2022",
+        to: "",
+        mainURL: 'https://netflix-jmg-clone.vercel.app/'
     },
     {
         name: "Portfolio",
@@ -127,12 +127,12 @@ export const projects = [
         mainURL: 'https://twitter-jmg.vercel.app/'
     },
     {
-        name: "Netflix Clone",
-        description: "Netflix clone using NextJs with powerful user authentication, subscriptions to different plans, payments with stripe, completely dynamic and responsive for all devices. You can also play real movie's trailers and add movies to your list",
-        image: "netflix.jpg",
-        from: "July 2022",
-        to: "",
-        mainURL: 'https://netflix-jmg-clone.vercel.app/'
+        name: "Pretty Dogs by Henry",
+        description: "Development of a SPA (Single Page Application) using React for the Front End and Redux as state management. The SPA obtains data from an API (through a Back End developed in Node.JS using Express), and also from a database managed with PostgreSQL.",
+        image: "landing.jpg",
+        from: "January 2022",
+        to: "February 2022",
+        mainURL: '/projects'
     },
     // {
     //     name: "Henry Shoes",
